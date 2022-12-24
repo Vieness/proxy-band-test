2aa2a775e7484ccd05b6d5e86b1cc71a1c4ef753
Vieness <ydvion13@gmail.com>
2022-12-24 15:02:45 +0200

    finish commit

--- src/features/albums/index.js
+++ src/features/albums/index.js
@@ -1,63 +1,30 @@
 import {useGetAlbumsQuery} from "./api";
-import {Box, Modal, Typography} from "@mui/material";
+import {Box, Dialog, DialogContent, DialogTitle, Typography} from "@mui/material";
+import Modal from '@mui/material/Modal';
 import React from "react";
 import {useSearchParams} from "react-router-dom";
 
-const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-};
 
-const Albums = ({handleClose, open}) => {
-    const [searchParams] = useSearchParams();
-    const {data} = useGetAlbumsQuery(searchParams.toString())
 
-    console.log(data);
+const Albums = ({selectedUser, handleClose, open}) => {
+    const {data = []} = useGetAlbumsQuery({userId: selectedUser}, {skip: !selectedUser})
+
     return (
-        <div>
-            <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-            >
-                <Box sx={style}>
-                    <Typography id="modal-modal-title" variant="h6" component="h2">
-                        asdfas
-                    </Typography>
-                    <Typography id="modal-modal-description" sx={{mt: 2}}>
-                        sdfasdf
+        <Dialog open={open} onClose={handleClose}>
+            <DialogTitle>
+                Albums
+            </DialogTitle>
+            <DialogContent>
+                {data.map(item=>
+                <Box>
+                    <Typography>
+                        {item.title}
                     </Typography>
                 </Box>
-            </Modal>
-            {/*            {data?.map(item =>
-                <Modal
-                    open={open}
-                    onClose={handleClose}
-                    aria-labelledby="modal-modal-title"
-                    aria-describedby="modal-modal-description"
-                >
-                    <Box sx={style}>
-                        <Typography id="modal-modal-title" variant="h6" component="h2">
-                            {item.userId}
-                        </Typography>
-                        <Typography id="modal-modal-description" sx={{mt: 2}}>
-                            {item.title}
-                        </Typography>
-                    </Box>
-                </Modal>
-            )}*/}
-
-
-        </div>
-    );
+                )}
+            </DialogContent>
+        </Dialog>
+    )
 };
 
 export default Albums;
--- src/features/users/index.js
+++ src/features/users/index.js
@@ -1,20 +1,31 @@
 import {Button, Card, CardActions, CardContent, Container, Typography} from "@mui/material";
 import {useGetUsersQuery} from "./api";
 import {useNavigate} from "react-router-dom";
-import React from "react";
+import {useState} from "react";
 import Albums from "../albums";
+import {useGetAlbumsQuery} from "../albums/api";
 
 const Users = () => {
+    const [open, setOpen] = useState(false);
+    const [selectedUser, setSelectedUser] = useState()
+
+
     const {data} = useGetUsersQuery();
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
+
+    const toggleDialog = () => {
+        setOpen(!open)
+    }
+    //user id
+    const handleSelectUser = (id) => () => {
+        setSelectedUser(id);
+        toggleDialog();
+
+    }
     const navigate = useNavigate();
-    console.log(open)
     return (
-        <div>
-            {data?.map(item =>
-                <Container key={item.id}>
+        <>
+            <Container>
+                {data?.map(item =>
                     <Card sx={{maxWidth: 250}}>
                         <CardContent>
                             <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
@@ -29,14 +40,13 @@ const Users = () => {
                         </CardContent>
                         <CardActions>
                             <Button onClick={() => navigate(`posts?userId=${item.id}`)} size="small">User Posts</Button>
-                            <Button onClick={handleOpen} size="small">User
-                                Albums</Button>
-                            {open && <Albums open={open} handleClose={handleClose}/>}
+                            <Button onClick={handleSelectUser(item.id)} size="small"> User Albums </Button>
                         </CardActions>
                     </Card>
-                </Container>
-            )}
-        </div>
+                )}
+            </Container>
+            <Albums selectedUser={selectedUser}  open={open} handleClose={toggleDialog}/>
+        </>
     );
 };
 
